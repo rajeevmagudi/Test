7df5f27b9541ac647fcd541f1b0dc4fee5fe6b4f
magudi <rajeevmagudi.75@gmail.com>
2025-04-20 16:05:21 +0530

    add product folder

---
+++ src/app.js
@@ -0,0 +1,6 @@
+function App(){
+    let msg = "awsome";
+    return <h2>This is s 1 {msg} component</h2>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+
+const header = <h1>This is First header</h1>;
+console.log(header);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(header);
+root.render(<App/>);
